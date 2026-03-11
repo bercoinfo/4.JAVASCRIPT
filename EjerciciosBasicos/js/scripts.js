4f0ce9f8c6289da278d950b4b145cda751aabeb6
josecr <josecarlos@infifive.com>
2026-03-11 18:53:01 +0100

    v1

---
+++ EjerciciosBasicos/js/scripts.js
@@ -0,0 +1,16 @@
+  // PRIMER EJERCICIO
+  function buscarNombre() {
+            let alineacion = ['Jose', 'Juan', 'Ana', 'Carmen', 'Patricia'];
+            let nombre = prompt("Indica el nombre que deseas comprobar");
+            let noEsta = 0;
+            for (let i = 0; i < alineacion.length; i++) {
+                if (nombre == alineacion[i]) {
+                    alert("El nombre se encuentra en la lista");
+                    noEsta = 1;
+                    break;} 
+                }
+            if(noEsta==0) {alert("El nombre no esta")}
+            }
+//SEGUNDO EJERCICIO
+
+        
